bf2fd33ff15495e89d9e9feb92554908113565dd
VD44 <vico.dusa@gmail.com>
2018-07-17 19:45:55 -0500

    Update rouge_l_tensorflow.py

--- rouge_l_tensorflow.py
+++ rouge_l_tensorflow.py
@@ -1,6 +1,6 @@
 # The MIT License
 #
-# Copyright (c) VD44
+# Copyright (c) 2018 VD44
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
